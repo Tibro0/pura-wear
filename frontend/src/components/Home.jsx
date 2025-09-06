6a7cad20140d5c9cf547fe1887ae0af7a622a031
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-09-07 01:43:41 +0600

    Common Layout Frontend

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,23 +1,16 @@
 import React from "react";
 import LatestProducts from "./common/LatestProducts";
 import FeaturedProducts from "./common/FeaturedProducts";
-import Header from "./common/Header";
-import Footer from "./common/Footer";
 import Hero from "./common/Hero";
+import Layout from "./common/Layout";
 
 const Home = () => {
   return (
-    <>
-      <Header/>
-
+    <Layout>
       <Hero/>
-
       <LatestProducts />
-
       <FeaturedProducts />
-
-      <Footer/>
-    </>
+    </Layout>
   );
 };
 
--- frontend/src/components/Shop.jsx
+++ frontend/src/components/Shop.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Layout from './common/Layout'
 
 const Shop = () => {
   return (
-    <div>
-      <h1>Shop Page</h1>
-    </div>
+    <Layout>
+      
+    </Layout>
   )
 }
 
