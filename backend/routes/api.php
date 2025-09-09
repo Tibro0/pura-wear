6fe4a28d6e9b38cc223112bf86e16bb7b2063eb5
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-09-10 01:50:57 +0600

    Category API CRUD

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/admin/login', [AuthController::class, 'authenticate']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::resource('categories', CategoryController::class);
+});
