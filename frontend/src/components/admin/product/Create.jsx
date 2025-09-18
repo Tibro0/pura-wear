5283767a2ce37c67b837b9a3bb38052889a2d13b
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-09-18 18:44:10 +0600

    Create Product UI Frontend

--- frontend/src/components/admin/product/Create.jsx
+++ frontend/src/components/admin/product/Create.jsx
@@ -1,11 +1,318 @@
-import React from 'react'
+import React, { useEffect, useState, useRef, useMemo } from "react";
+import Layout from "../../common/Layout";
+import { Link, useNavigate } from "react-router-dom";
+import Sidebar from "../../common/Sidebar";
+import { useForm } from "react-hook-form";
+import { adminToken, apiUrl } from "../../common/http";
+import { toast } from "react-toastify";
+import JoditEditor from "jodit-react";
 
-const Create = () => {
+const Create = ({ placeholder }) => {
+  // Page Title
+  useEffect(() => {
+    document.title = "Pura Wear | Create Product";
+  }, []);
+
+  const editor = useRef(null);
+  const [content, setContent] = useState("");
+  const [disable, setDisable] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [brands, setBrands] = useState([]);
+  const navigate = useNavigate();
+
+  const config = useMemo(
+    () => ({
+      readonly: false, // all options from https://xdsoft.net/jodit/docs/,
+      placeholder: placeholder || "",
+    }),
+    [placeholder]
+  );
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const saveProduct = async (data) => {
+    setDisable(true);
+    const res = await fetch(`${apiUrl}/products`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${adminToken()}`,
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setDisable(false);
+        if (result.status == 200) {
+          toast.success(result.message);
+          navigate("/admin/categories");
+        } else {
+          console.log("Something Went Wrong!");
+        }
+      });
+  };
+
+  const fetchCategories = async () => {
+    const res = await fetch(`${apiUrl}/categories`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${adminToken()}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setCategories(result.data);
+      });
+  };
+
+  const fetchBrands = async () => {
+    const res = await fetch(`${apiUrl}/brands`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${adminToken()}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setBrands(result.data);
+      });
+  };
+
+  useEffect(() => {
+    fetchCategories();
+    fetchBrands();
+  }, []);
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Layout>
+      <div className="container">
+        <div className="row">
+          <div className="d-flex justify-content-between mt-5 pb-3">
+            <h4 className="h4 pb-0 mb-0">Products / Create</h4>
+            <Link to="/admin/products" className="btn btn-primary">
+              Back
+            </Link>
+          </div>
+          <div className="col-md-3">
+            <Sidebar />
+          </div>
+          <div className="col-md-9">
+            <form onSubmit={handleSubmit(saveProduct)}>
+              <div className="card shadow">
+                <div className="card-body p-4">
+                  <div className="mb-3">
+                    <label htmlFor="" className="form-label">
+                      Title
+                    </label>
+                    <input
+                      {...register("title", {
+                        required: "The Title Filed is Required.",
+                      })}
+                      type="text"
+                      className={`form-control ${errors.title && "is-invalid"}`}
+                      placeholder="Title"
+                    />
+                    {errors.title && (
+                      <p className="invalid-feedback">{errors.name.message}</p>
+                    )}
+                  </div>
 
-export default Create
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <label htmlFor="" className="form-label">
+                          Category
+                        </label>
+                        <select className="form-select">
+                          <option value="">Select a Category</option>
+                          {categories &&
+                            categories.map((category) => {
+                              return (
+                                <option
+                                  key={`category-${category.id}`}
+                                  value={category.id}
+                                >
+                                  {category.name}
+                                </option>
+                              );
+                            })}
+                        </select>
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <label htmlFor="" className="form-label">
+                          Brand
+                        </label>
+                        <select className="form-select">
+                          <option value="">Select a Brand</option>
+                          {brands &&
+                            brands.map((brand) => {
+                              return (
+                                <option
+                                  key={`brand-${brand.id}`}
+                                  value={brand.id}
+                                >
+                                  {brand.name}
+                                </option>
+                              );
+                            })}
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="mb-3">
+                    <label htmlFor="" className="form-label">
+                      Short Description
+                    </label>
+                    <textarea
+                      className="form-control"
+                      placeholder="Short Description"
+                      rows={4}
+                    ></textarea>
+                  </div>
+
+                  <div className="mb-3">
+                    <label htmlFor="" className="form-label">
+                      Description
+                    </label>
+                    <JoditEditor
+                      ref={editor}
+                      value={content}
+                      config={config}
+                      tabIndex={1} // tabIndex of textarea
+                      onBlur={(newContent) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+                    />
+                  </div>
+
+                  <h3 className="py-3 border-bottom mb-3">Pricing</h3>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <label htmlFor="" className="form-label">
+                          Price
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Price"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <label htmlFor="" className="form-label">
+                          Discounted Price
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Discounted Price"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <h3 className="py-3 border-bottom mb-3">Inventory</h3>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <label htmlFor="" className="form-label">
+                          SKU
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="SKU"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <label htmlFor="" className="form-label">
+                          Barcode
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Barcode"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <label htmlFor="" className="form-label">
+                          Qty
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Qty"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <label htmlFor="" className="form-label">
+                          Status
+                        </label>
+                        <select
+                          {...register("status", {
+                            required: "Please Select a Status.",
+                          })}
+                          className={`form-select ${
+                            errors.status && "is-invalid"
+                          }`}
+                        >
+                          <option value="">Select a Status</option>
+                          <option value="1">Active</option>
+                          <option value="0">Block</option>
+                        </select>
+                        {errors.status && (
+                          <p className="invalid-feedback">
+                            {errors.status.message}
+                          </p>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+
+                  <h3 className="py-3 border-bottom mb-3">Gallery</h3>
+                  <div className="mb-3">
+                    <label htmlFor="" className="form-label">
+                      Image
+                    </label>
+                    <input type="file" className="form-control" />
+                  </div>
+                </div>
+              </div>
+              <button
+                disabled={disable}
+                type="submit"
+                className="btn btn-primary mt-3 mb-5"
+              >
+                Create
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Create;
--- frontend/src/components/admin/product/Show.jsx
+++ frontend/src/components/admin/product/Show.jsx
@@ -46,8 +46,8 @@ const Show = () => {
         <div className="row">
           <div className="d-flex justify-content-between mt-5 pb-3">
             <h4 className="h4 pb-0 mb-0">Products</h4>
-            <Link to="" className="btn btn-primary">
-              Button
+            <Link to="/admin/products/create" className="btn btn-primary">
+              Create
             </Link>
           </div>
           <div className="col-md-3">
