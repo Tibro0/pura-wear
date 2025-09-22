a32e30e316a69121f7074b866dcb317dbc54b522
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-09-22 15:47:28 +0600

    Show Latest Products Frontend

--- frontend/src/components/common/LatestProducts.jsx
+++ frontend/src/components/common/LatestProducts.jsx
@@ -1,73 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductImg from "../../assets/images/eight.jpg";
 import { Link } from "react-router-dom";
+import { apiUrl } from "./http";
 
 const LatestProducts = () => {
+  const [products, setProducts] = useState([]);
+
+  const latestProducts = async () => {
+    await fetch(apiUrl + "/get-latest-products", {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setProducts(result.data);
+      });
+  };
+
+  useEffect(() => {
+    latestProducts();
+  }, []);
+
   return (
     <section className="section-2 pt-5">
       <div className="container">
         <h2>New Arrivals</h2>
         <div className="row mt-4">
-          <div className="col-md-3 col-6">
-            <div className="product card border-0">
-              <div className="card-img">
-                <Link to="/product">
-                  <img src={ProductImg} alt="" className="w-100" />
-                </Link>
-              </div>
-              <div className="card-body pt-3">
-                <Link to="/product">Red Check Shirt foe Men</Link>
-                <div className="price">
-                  $50 <span className="text-decoration-line-through">$80</span>
+          {products &&
+            products.map((product) => {
+              return (
+                <div className="col-md-3 col-6">
+                  <div className="product card border-0">
+                    <div className="card-img">
+                      <Link to="/product">
+                        {product.image_url == "" ? (
+                          <img src="https://placehold.co/315x362" />
+                        ) : (
+                          <img src={product.image_url} className="w-100" />
+                        )}
+                      </Link>
+                    </div>
+                    <div className="card-body pt-3">
+                      <Link to="/product">{product.title}</Link>
+                      <div className="price">
+                        ${product.price}&nbsp;
+                        {product.compare_price && (
+                          <span className="text-decoration-line-through">
+                            ${product.compare_price}
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3 col-6">
-            <div className="product card border-0">
-              <div className="card-img">
-                <Link to="/product">
-                  <img src={ProductImg} alt="" className="w-100" />
-                </Link>
-              </div>
-              <div className="card-body pt-3">
-                <Link to="/product">Red Check Shirt foe Men</Link>
-                <div className="price">
-                  $50 <span className="text-decoration-line-through">$80</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3 col-6">
-            <div className="product card border-0">
-              <div className="card-img">
-                <Link to="/product">
-                  <img src={ProductImg} alt="" className="w-100" />
-                </Link>
-              </div>
-              <div className="card-body pt-3">
-                <Link to="/product">Red Check Shirt foe Men</Link>
-                <div className="price">
-                  $50 <span className="text-decoration-line-through">$80</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3 col-6">
-            <div className="product card border-0">
-              <div className="card-img">
-                <Link to="/product">
-                  <img src={ProductImg} alt="" className="w-100" />
-                </Link>
-              </div>
-              <div className="card-body pt-3">
-                <Link to="/product">Red Check Shirt foe Men</Link>
-                <div className="price">
-                  $50 <span className="text-decoration-line-through">$80</span>
-                </div>
-              </div>
-            </div>
-          </div>
+              );
+            })}
         </div>
       </div>
     </section>
