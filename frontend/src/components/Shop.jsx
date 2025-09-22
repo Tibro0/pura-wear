a12a861f4bd0a13102db8ab9638f18cee0924c59
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-09-23 00:22:41 +0600

    Show Categories & Brands in Frontend

--- frontend/src/components/Shop.jsx
+++ frontend/src/components/Shop.jsx
@@ -1,18 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "./common/Layout";
 import ProductImg from "../assets/images/eight.jpg";
 import { Link } from "react-router-dom";
+import { apiUrl } from "./common/http";
 
 const Shop = () => {
+  // Page Title
+  useEffect(() => {
+    document.title = "Pura Wear | Shop";
+  }, []);
+
+  const [categories, setCategories] = useState([]);
+  const [brands, setBrands] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const fetchCategories = () => {
+    fetch(`${apiUrl}/get-categories`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.status == 200) {
+          setCategories(result.data);
+        } else {
+          console.log("Something Went Wrong!");
+        }
+      });
+  };
+
+  const fetchBrands = () => {
+    fetch(`${apiUrl}/get-brands`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.status == 200) {
+          setBrands(result.data);
+        } else {
+          console.log("Something Went Wrong!");
+        }
+      });
+  };
+
+  useEffect(() => {
+    fetchCategories();
+    fetchBrands();
+  }, []);
+
   return (
     <Layout>
       <div className="container">
         <nav aria-label="breadcrumb" className="py-4">
-          <ol class="breadcrumb">
-            <li class="breadcrumb-item">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
               <Link to="#">Home</Link>
             </li>
-            <li class="breadcrumb-item active" aria-current="page">
+            <li className="breadcrumb-item active" aria-current="page">
               Shop
             </li>
           </ol>
@@ -23,24 +74,17 @@ const Shop = () => {
               <div className="card-body p-4">
                 <h3 className="mb-3">Categories</h3>
                 <ul>
-                  <li className="mb-2">
-                    <input type="checkbox" />
-                    <label htmlFor="" className="ps-2">
-                      Kids
-                    </label>
-                  </li>
-                  <li className="mb-2">
-                    <input type="checkbox" />
-                    <label htmlFor="" className="ps-2">
-                      Mens
-                    </label>
-                  </li>
-                  <li className="mb-2">
-                    <input type="checkbox" />
-                    <label htmlFor="" className="ps-2">
-                      Women
-                    </label>
-                  </li>
+                  {categories &&
+                    categories.map((category) => {
+                      return (
+                        <li className="mb-2" key={`category-${category.id}`}>
+                          <input type="checkbox" value={category.id} />
+                          <label htmlFor="" className="ps-2">
+                            {category.name}
+                          </label>
+                        </li>
+                      );
+                    })}
                 </ul>
               </div>
             </div>
@@ -49,30 +93,17 @@ const Shop = () => {
               <div className="card-body p-4">
                 <h3 className="mb-3">Brands</h3>
                 <ul>
-                  <li className="mb-2">
-                    <input type="checkbox" />
-                    <label htmlFor="" className="ps-2">
-                      Puma
-                    </label>
-                  </li>
-                  <li className="mb-2">
-                    <input type="checkbox" />
-                    <label htmlFor="" className="ps-2">
-                      Killer
-                    </label>
-                  </li>
-                  <li className="mb-2">
-                    <input type="checkbox" />
-                    <label htmlFor="" className="ps-2">
-                      Levis
-                    </label>
-                  </li>
-                  <li className="mb-2">
-                    <input type="checkbox" />
-                    <label htmlFor="" className="ps-2">
-                      Flying Machine
-                    </label>
-                  </li>
+                  {brands &&
+                    brands.map((brand) => {
+                      return (
+                        <li className="mb-2" key={`brand-${brand.id}`}>
+                          <input type="checkbox" value={brand.id} />
+                          <label htmlFor="" className="ps-2">
+                            {brand.name}
+                          </label>
+                        </li>
+                      );
+                    })}
                 </ul>
               </div>
             </div>
