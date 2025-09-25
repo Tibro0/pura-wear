2695db02364b288f160420c21727a4f7da6f666f
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-09-25 15:05:26 +0600

    Create OrderItem Table

--- backend/database/migrations/2025_09_25_085836_create_order_items_table.php
+++ backend/database/migrations/2025_09_25_085836_create_order_items_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('size');
+            $table->double('price', 10, 2);
+            $table->double('unit_price', 10, 2);
+            $table->integer('qty');
             $table->timestamps();
         });
     }
