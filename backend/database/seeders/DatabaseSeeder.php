055e2436527ab26c2977c329edc98a60bcdf4a9b
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2026-03-15 01:19:36 +0600

    Product Seeder Add Backend

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -13,18 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
          $this->call([
             UserSeeder::class,
             CategorySeeder::class,
             BrandSeeder::class,
-            // ProductSeeder::class,
+            ProductSeeder::class,
             SizeSeeder::class,
             // ProductImageSeeder::class,
             // ProductSizeSeeder::class,
